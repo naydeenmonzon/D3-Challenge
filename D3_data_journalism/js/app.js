242391f39e55ca5648e0722263531ca8ca691fcc
naydeenmonzon <naydeen.monzon@icloud.com>
2021-05-31 22:07:21 -0700

    commit

---
+++ D3_data_journalism/js/app.js
@@ -0,0 +1,54 @@
+// @TODO: YOUR CODE HERE!
+
+url="data/data.csv"
+
+var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    width = 960 - margin.left - margin.right,
+    height = 640 - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+var svg = d3.select('body').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
+ const dataset = d3.csv(url, function(error, data) {
+    if (error) throw error;
+
+    var x = d3.scaleLinear()
+    .domain([0, 4000])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  var y = d3.scaleLinear()
+    .domain([0, 500000])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.GrLivArea); } )
+      .attr("cy", function (d) { return y(d.SalePrice); } )
+      .attr("r", 1.5)
+      .style("fill", "#69b3a2")
+
+      return data
+})
+
+console.log(dataset)
